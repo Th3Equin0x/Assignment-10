6241d2e262626e8cb8343b17a3c01b5a47a964e8
Th3Equin0x <sgruver22@gmail.com>
2019-11-13 21:06:12 -0600

    Did the thing

---
+++ Assignment-10/Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    private float _speed;
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    static public int Count = 0;
+
+    private float _attack;
+    public float Attack
+    {
+        get { return _attack; }
+        set { _attack = value; }
+    }
+
+    public Enemy()
+    {
+        Name = "Enemy #" + Count;
+        Speed = 1;
+        Count++;
+    }
+
+    public Enemy(string name, float speed)
+    {
+        Name = name;
+        Speed = speed;
+        Count++;
+    }
+
+    public override string ToString()
+    {
+        return "Enemy: " + Name + " " + Speed + " " + Attack;
+    }
+}
---
+++ Assignment-10/Assets/Scripts/EnemyDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDatabase : MonoBehaviour
+{
+    public Dictionary<string, Enemy> Enemies = new Dictionary<string, Enemy>();
+
+    private void Start()
+    {
+        Enemies.Add("generic enemy", new Enemy());
+        Enemies.Add("fast enemy", new Enemy("Speedy", 2f));
+        Enemies.Add("slow enemy", new Enemy("Tanky", 0.5f));
+
+        if (Enemies.ContainsKey("generic enemy"))
+        {
+            Enemies.Remove("generic enemy");
+        }
+
+        foreach (KeyValuePair<string, Enemy> E in Enemies)
+        {
+            Debug.Log(E.Key + " = " + E.Value.ToString());
+        }
+    }
+}
